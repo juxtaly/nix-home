7859364dd75f267dc159368b48d92596ecd2d521
atriw <875241499@qq.com>
2022-04-14 02:56:09 +0800

    Restore colorscheme

--- nvim/init_lua.lua
+++ nvim/init_lua.lua
@@ -1,7 +1,7 @@
 require "user.options"
 require "user.keymaps"
 require "user.treesitter"
--- require "user.colorscheme"
+require "user.colorscheme"
 require "user.cmp"
 require "user.lsp"
 require "user.telescope"

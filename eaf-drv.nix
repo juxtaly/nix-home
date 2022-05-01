4cf175b1269d165f6acfbaf574bd0181a4aafce8
atriw <875241499@qq.com>
2022-05-02 02:48:13 +0800

    Add eaf pdf viewer

--- eaf-drv.nix
+++ eaf-drv.nix
@@ -13,6 +13,12 @@ let
     rev = "b8eafe9fc72fa857364f1dee10e5c4539ef58c7f";
     sha256 = "sha256-leVgH7ABWNvQUBeLfWKUEMsHPwBWWHsX0pbEdI4ftN4=";
   };
+  eaf-pdf-viewer = pkgs.fetchFromGitHub {
+    owner = "emacs-eaf";
+    repo = "eaf-pdf-viewer";
+    rev = "ce363fb928873827d2065b4598f1d86290f27cc5";
+    sha256 = "sha256-51TUPotl2VpOst6R+tdbWiSYgg13uGdFBJzjRZ+NMwg=";
+  };
   eaf-browser = (pkgs.callPackage (pkgs.fetchFromGitHub {
     owner = "atriw";
     repo = "eaf-browser";
@@ -24,13 +30,15 @@ stdenv.mkDerivation {
   pname = "emacs-application-framework";
   inherit version;
   src = eaf;
-  buildInputs = [eaf-browser eaf-demo];
+  buildInputs = [eaf-browser eaf-pdf-viewer eaf-demo];
   dontBuild = true;
   installPhase = ''
     mkdir -p $out/
     cp -r $src/* $out/
     mkdir -p $out/app/eaf-browser
     cp -r ${eaf-browser}/lib/node_modules/eaf-browser/* $out/app/eaf-browser/
+    mkdir -p $out/app/eaf-pdf-viewer
+    cp -r ${eaf-pdf-viewer}/* $out/app/eaf-pdf-viewer
     mkdir -p $out/app/eaf-demo
     cp -r ${eaf-demo}/* $out/app/eaf-demo/
   '';

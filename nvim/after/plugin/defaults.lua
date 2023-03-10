46337fd0f2631b259daf417fdea08646e776572b
atriw <875241499@qq.com>
2023-03-11 05:31:08 +0800

    Neovide

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -1,5 +1,26 @@
 vim.opt.relativenumber = false
 vim.opt.cursorline = true
+vim.o.guifont = "Rec Mono Casual,Fira Code Nerd Font:h18"
+vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 0
+vim.g.neovide_padding_left = 0
+-- vim.g.neovide_fullscreen = false
+if vim.g.neovide then
+  vim.keymap.set({'n', 'v', 'i'}, '<M-enter>', function ()
+    vim.g.neovide_fullscreen = (not vim.g.neovide_fullscreen)
+  end, { desc = 'Toggle neovide fullscreen', silent = true})
+end
+
+vim.keymap.set('n', '<leader><leader>r', function ()
+  vim.cmd([[
+    source $MYVIMRC
+    runtime! plugin/**/*.vim
+    runtime! plugin/**/*.lua
+    runtime! after/**/*.vim
+    runtime! after/**/*.lua
+  ]])
+end, { desc = 'Reload configs and plugins', silent = true})
 
 -- ******************************
 -- Toggleterm.nvim Configurations
